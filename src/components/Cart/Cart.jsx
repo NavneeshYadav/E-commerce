82b1029559a517beced16a0c966b6c4b40231768
Navneesh Yadav <yadavnavneesh999@gmail.com>
2023-04-20 21:31:51 +0530

    adding cart feature

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Cart.scss"
+const Cart = () => {
+  return (
+    <div className='cart'>
+        <h1>Products in your cart</h1>
+    </div>
+  )
+}
+
+export default Cart
