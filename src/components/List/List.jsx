aa49a92720e3d3e3b94942ccad4a7453cad3b164
Navneesh Yadav <yadavnavneesh999@gmail.com>
2023-04-20 21:12:16 +0530

    second commit

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -40,8 +40,8 @@ const List = () => {
   return (
     <div className="list">
       {data?.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        <Card item={item} key={item.id} />
+      ))}
     </div>
   );
 };
